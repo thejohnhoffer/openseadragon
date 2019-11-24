98d7abf6b4766349817e358b967d9dd992689a09
rmtoth <robert.m.toth@gmail.com>
2019-11-24 22:19:56 +0100

    at least got the uint texelFetch sampler to work

--- src/webgldrawer.js
+++ src/webgldrawer.js
@@ -86,25 +86,29 @@ $.WebGlDrawer = function( options ) {
         precision highp float;                                              \
         precision highp int;                                                \
         precision highp sampler2DArray;                                    \
+        precision highp usampler2D;                                    \
                                                                     \
         in highp vec2 vTextureCoord;                  \
         out vec4 color;                                         \
         uniform ivec2 uSize;                                          \
         uniform int uTile;                                           \
-        uniform sampler2DArray uSampler;                            \
+        uniform sampler2DArray uTextureSampler;                            \
+        uniform usampler2D uTileSampler;                            \
                                                                     \
         void main(void) {                                             \
-            color = texture(uSampler, vec3(vTextureCoord, uTile));      \
+            ivec2 c = ivec2(int(gl_FragCoord.x ), int(gl_FragCoord.y ));            \
+            uint d =  uvec4(texelFetch(uTileSampler, ivec2(2, 2), 0)).x;          \
+            color = vec4(float(d) / 255.0, 0.0, 0.0, 1.0);      \
         }                                                           \
     ";
-    // texture(uSampler, vec3(vTextureCoord, uTile));
 
     this.program = this._loadProgram();
 
     this.vertexPos = this.gl.getAttribLocation(this.program, "aVertexPos");
     this.textureCoord = this.gl.getAttribLocation(this.program, "aTextureCoord");
 
-    this.sampler = this.gl.getUniformLocation(this.program, "uSampler");
+    this.textureSampler = this.gl.getUniformLocation(this.program, "uTextureSampler");
+    this.tileSampler = this.gl.getUniformLocation(this.program, "uTileSampler");
     this.uSize = this.gl.getUniformLocation(this.program, "uSize");
     this.uTile = this.gl.getUniformLocation(this.program, "uTile");
 
@@ -139,9 +143,50 @@ $.WebGlDrawer.prototype = {
 
         this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
 
+        this.gl.useProgram(this.program);
 
-        this._loadTexture(tiles);
+        // this._loadTexture(tiles);
+        var texture = this._loadTilePositionTexture(tiles);
 
+        var data = [
+            -1, -1,      // lower left
+            1, -1,  // lower right
+            -1, 1,          // upper left
+            1, 1       // upper right
+        ];
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
+        // TODO ES6?
+        // eslint-disable-next-line no-undef
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(data), this.gl.DYNAMIC_DRAW);
+
+        var numComponents = 2;
+        var type = this.gl.FLOAT;
+        var normalize = false;
+        var stride = 0;
+        var offset = 0;
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
+        this.gl.vertexAttribPointer(
+            this.vertexPos,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        this.gl.enableVertexAttribArray(this.vertexPos);
+
+        // this.gl.uniform2i(this.uSize, this.canvas.width, this.canvas.height);
+        // this.gl.uniform1i(this.uTile, 0);
+        // this.gl.uniform1i(this.textureSampler, 0);
+        this.gl.uniform1i(this.tileSampler, 0);
+
+        this.gl.activeTexture(this.gl.TEXTURE0);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+
+        var vertexCount = 4;
+        offset = 0;
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, offset, vertexCount);
+
+        /*
         for (var i = 0; i < tiles.length; i++) {
             var tile = tiles[i];
             var dest = tile.getDestinationRect(scale, translate);
@@ -190,16 +235,16 @@ $.WebGlDrawer.prototype = {
                 offset);
             this.gl.enableVertexAttribArray(this.textureCoord);
 
-            this.gl.useProgram(this.program);
-
             this.gl.uniform2i(this.uSize, this.canvas.width, this.canvas.height);
             this.gl.uniform1i(this.uTile, i);
-            this.gl.uniform1i(this.sampler, 0);
+            // this.gl.uniform1i(this.textureSampler, 0);
+            this.gl.uniform1i(this.tileSampler, 0);
 
             var vertexCount = 4;
             offset = 0;
             this.gl.drawArrays(this.gl.TRIANGLE_STRIP, offset, vertexCount);
         }
+        */
 
 
         // Create texture array with all tile textures
@@ -260,6 +305,7 @@ $.WebGlDrawer.prototype = {
     _loadTexture: function( tiles ) {
 
         var texture = this.gl.createTexture();
+        this.gl.activeTexture(this.gl.TEXTURE0);
         this.gl.bindTexture(this.gl.TEXTURE_2D_ARRAY, texture);
 
         for (var i = 0; i < tiles.length; i++) {
@@ -288,6 +334,43 @@ $.WebGlDrawer.prototype = {
         this.gl.texParameteri(this.gl.TEXTURE_2D_ARRAY, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
         this.gl.texParameteri(this.gl.TEXTURE_2D_ARRAY, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
         this.gl.texParameteri(this.gl.TEXTURE_2D_ARRAY, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+        return texture;
+    },
+
+    _loadTilePositionTexture: function( tiles ) {
+        var texture = this.gl.createTexture();
+        this.gl.activeTexture(this.gl.TEXTURE0);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+
+        var width = this.canvas.width;
+        var height = this.canvas.height;
+        // eslint-disable-next-line no-undef
+        var data = new Uint8Array(width * height);
+        for (var i = 0; i < tiles.length; i++) {
+            var tile = tiles[i];
+            var context = tile.getContext();
+            var bounds = tile.limitSourceBounds(context.canvas);
+            for (var x = bounds.x; x < bounds.x + bounds.width; x++) {
+                for (var y = bounds.y; y < bounds.y + bounds.height; y++) {
+                    // TODO x and y may be outside bounds
+                    data[x * width + y] = 1;
+                }
+            }
+        }
+        // TODO is not red on screen
+        data.fill(180);
+        var level = 0;
+        var border = 0;
+        var internalFormat = this.gl.R8UI;
+        var format =  this.gl.RED_INTEGER;
+        var offset = 0;
+        var type = this.gl.UNSIGNED_BYTE;
+        this.gl.texImage2D(this.gl.TEXTURE_2D, level, internalFormat, width, height, border, format, type, data, offset);
+
+        // this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+        // this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
         return texture;
     }
 };
