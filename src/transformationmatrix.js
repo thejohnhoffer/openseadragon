d5704ce3cb0480ae44455ea604f23ab70f25e7a6
rmtoth <robert.m.toth@gmail.com>
2020-01-11 14:03:26 +0100

    rename

--- src/transformationmatrix.js
+++ src/transformationmatrix.js
@@ -1,5 +1,5 @@
 /*
- * OpenSeadragon - TransformMatrix
+ * OpenSeadragon - TransformaationMatrix
  *
  * Copyright (C) 2009 CodePlex Foundation
  * Copyright (C) 2010-2013 OpenSeadragon contributors
@@ -35,8 +35,8 @@
 (function ($) {
 
     /**
-     * @class TransformMatrix
-     * @classdesc A TransformMatrix is a 2 by 3 affine 2D transform matrix with
+     * @class TransformaationMatrix
+     * @classdesc A TransformaationMatrix is a 2 by 3 affine 2D transform matrix with
      * methods for applying rotation, scaling and translating. Element
      * notation is a_(row, col) and starts at one, i.e. a12 is the first row and
      * second column. The matrix defaults as the identity matrix.
@@ -49,55 +49,55 @@
      * @param {Number} [a22] Row 2, column 2.
      * @param {Number} [a23] Row 2, column 3.
      */
-    $.TransformMatrix = function (a11, a12, a13, a21, a22, a23) {
+    $.TransformaationMatrix = function (a11, a12, a13, a21, a22, a23) {
         /**
          * The vector component 'a11'.
          * @member {Number} a11
-         * @memberof OpenSeadragon.TransformMatrix#
+         * @memberof OpenSeadragon.TransformaationMatrix#
          */
         this.a11 = typeof (a11) == "number" ? a11 : 1;
         /**
              * The vector component 'a12'.
              * @member {Number} a12
-             * @memberof OpenSeadragon.TransformMatrix#
+             * @memberof OpenSeadragon.TransformaationMatrix#
              */
         this.a12 = typeof (a12) == "number" ? a12 : 0;
         /**
              * The vector component 'a13'.
              * @member {Number} a13
-             * @memberof OpenSeadragon.TransformMatrix#
+             * @memberof OpenSeadragon.TransformaationMatrix#
              */
         this.a13 = typeof (a13) == "number" ? a13 : 0;
 
         /**
          * The vector component 'a21'.
          * @member {Number} a21
-         * @memberof OpenSeadragon.TransformMatrix#
+         * @memberof OpenSeadragon.TransformaationMatrix#
          */
         this.a21 = typeof (a21) == "number" ? a21 : 0;
         /**
              * The vector component 'a22'.
              * @member {Number} a22
-             * @memberof OpenSeadragon.TransformMatrix#
+             * @memberof OpenSeadragon.TransformaationMatrix#
              */
         this.a22 = typeof (a22) == "number" ? a22 : 1
         /**
              * The vector component 'a23'.
              * @member {Number} a23
-             * @memberof OpenSeadragon.TransformMatrix#
+             * @memberof OpenSeadragon.TransformaationMatrix#
              */
         this.a23 = typeof (a23) == "number" ? a23 : 0;
 
     };
 
-    /** @lends OpenSeadragon.TransformMatrix.prototype */
-    $.TransformMatrix.prototype = {
+    /** @lends OpenSeadragon.TransformaationMatrix.prototype */
+    $.TransformaationMatrix.prototype = {
         /**
          * @function
-         * @returns {OpenSeadragon.TransformMatrix} a duplicate of this TransformMatrix
+         * @returns {OpenSeadragon.TransformaationMatrix} a duplicate of this TransformaationMatrix
          */
         clone: function () {
-            return new $.TransformMatrix(this.a11, this.a12, this.a13, this.a21, this.a22, this.a23);
+            return new $.TransformaationMatrix(this.a11, this.a12, this.a13, this.a21, this.a22, this.a23);
         },
 
         /**
